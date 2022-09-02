e423e503aa4414335332083468fefa8fa6820675
Erkin Kubatov <qoobatov@gmail.com>
2022-09-02 18:50:06 +0600

    fetch prormo content

--- src/pages/Promo/Promo.jsx
+++ src/pages/Promo/Promo.jsx
@@ -1,139 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Promo.css";
-import NewsImages from "../../Images/img1.png";
-import NewsImages2 from "../../Images/img2.png";
-import NewsImages3 from "../../Images/img3.png";
+import Promocard from "./pormocomponent/Promocard";
+
 export default function Promo() {
+  const [sales, setSales] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://still-forest-37543.herokuapp.com/sale")
+      .then((res) => {
+        console.log(res);
+        setSales(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log("sales", sales);
   return (
     <div className="allPromoNews">
       <div className="promoBlock">
         <div className="promoAndNews">
-          <h2 className="promoH2">
+          <h1 className="promoH2">
             Акции и новости <br /> в заведениях Бишкека
-          </h2>
-          <p className="promoMonth">июнь-июль 2022</p>
+          </h1>
+          
         </div>
-        <div className="newAndDate">
-          <p className="newDate">24 июня 2022 в 13.00</p>
-          <a href="#" className="newSierra">
-            Sierra coffee
-          </a>
-          <p className="newSpeciality">НОВИНКА</p>
-        </div>
-        <div className="UnderText">
-          <div className="newsImages">
-            <img className="newsImage1" src={NewsImages} />
-            <img className="newsImage2" src={NewsImages2} />
-            <img className="newsImage3" src={NewsImages3} />
-          </div>
-          <p className="newsText">
-            А ещё предприниматели в сети интернет освещают чрезвычайно
-            интересные особенности картины в целом, однако конкретные выводы,
-            разумеется, превращены в посмешище, хотя само их существование
-            приносит несомненную пользу обществу. Не следует, однако, забывать,
-            что глубокий уровень погружения играет важную роль в формировании
-            экспериментов, поражающих по своей масштабности и грандиозности.
-            Может быть так не должно было{" "}
-          </p>
-          <div className="newsButton">
-            <button className="newsBtn">Показать полностью</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="promoBlock">
-        <div className="newAndDate">
-          <p className="newDate">24 июня 2022 в 13.00</p>
-          <a href="#" className="newSierra">
-            Sierra coffee
-          </a>
-
-          <p className="newSpeciality">НОВИНКА</p>
-        </div>
-        <div className="UnderText">
-          <div className="newsImages">
-            <img className="newsImage1" src={NewsImages} />
-            <img className="newsImage2" src={NewsImages2} />
-            <img className="newsImage3" src={NewsImages3} />
-          </div>
-          <p className="newsText">
-            А ещё предприниматели в сети интернет освещают чрезвычайно
-            интересные особенности картины в целом, однако конкретные выводы,
-            разумеется, превращены в посмешище, хотя само их существование
-            приносит несомненную пользу обществу. Не следует, однако, забывать,
-            что глубокий уровень погружения играет важную роль в формировании
-            экспериментов, поражающих по своей масштабности и грандиозности.
-            Может быть так не должно было{" "}
-          </p>
-          <div className="newsButton">
-            <button className="newsBtn">Показать полностью</button>
-          </div>
-        </div>
-      </div>
-      <div className="promoBlock">
-        <div className="newAndDate">
-          <p className="newDate">24 июня 2022 в 13.00</p>
-          <a href="#" className="newSierra">
-            Sierra coffee
-          </a>
-
-          <p className="newSpeciality">НОВИНКА</p>
-        </div>
-        <div className="UnderText">
-          <div className="newsImages">
-            <img className="newsImage1" src={NewsImages} />
-            <img className="newsImage2" src={NewsImages2} />
-            <img className="newsImage3" src={NewsImages3} />
-          </div>
-          <p className="newsText">
-            А ещё предприниматели в сети интернет освещают чрезвычайно
-            интересные особенности картины в целом, однако конкретные выводы,
-            разумеется, превращены в посмешище, хотя само их существование
-            приносит несомненную пользу обществу. Не следует, однако, забывать,
-            что глубокий уровень погружения играет важную роль в формировании
-            экспериментов, поражающих по своей масштабности и грандиозности.
-            Может быть так не должно было{" "}
-          </p>
-          <div className="newsButton">
-            <button className="newsBtn">Показать полностью</button>
-          </div>
-        </div>
-      </div>
-      <div className="promoBlock">
-        <div className="newAndDate">
-          <p className="newDate">24 июня 2022 в 13.00</p>
-          <a href="#" className="newSierra">
-            Sierra coffee
-          </a>
-
-          <p className="newSpeciality">НОВИНКА</p>
-        </div>
-        <div className="UnderText">
-          <div className="newsImages">
-            <img className="newsImage1" src={NewsImages} />
-            <img className="newsImage2" src={NewsImages2} />
-            <img className="newsImage3" src={NewsImages3} />
-          </div>
-          <p className="newsText">
-            А ещё предприниматели в сети интернет освещают чрезвычайно
-            интересные особенности картины в целом, однако конкретные выводы,
-            разумеется, превращены в посмешище, хотя само их существование
-            приносит несомненную пользу обществу. Не следует, однако, забывать,
-            что глубокий уровень погружения играет важную роль в формировании
-            экспериментов, поражающих по своей масштабности и грандиозности.
-            Может быть так не должно было{" "}
-          </p>
-          <div className="newsButton">
-            <button className="newsBtn">Показать полностью</button>
-          </div>
-        </div>
-      </div>
-      <div className="discount">
-        <div className="take-discount">
-          <p className="sale-discount">Весь июль со скидкой в 50%! </p>
-          <p className="bring-cup">Принеси с собой кружку и получи свой кофе с 50% скидкой! </p>
-        </div>
-        <a href="#">Как добраться</a>
+        {sales.map((sale) => (
+          <Promocard
+            date={sale.time_create}
+            time={sale.time_update}
+            rest_name={sale.rest_name}
+            image={sale.sale_image}
+            description={sale.text}
+          />
+        ))}
       </div>
     </div>
   );
